9e3adb93da2afd62cc11efde95808867bf5c4fb1
abdulquayumhp <badulquayumhp998@gmailc.com>
2022-10-26 01:05:39 +0600

    live server add

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require("express");
 
 const app = express();
 
+const cors = require("cors");
+app.use(cors());
+
 const port = process.env.port || 5000;
+
+const courses = require("./data/corses.json");
 
 app.get("/", (req, res) => {
 	res.send("news api running");
