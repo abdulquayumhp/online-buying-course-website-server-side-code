03653b916124711cf5cb81215c290293e52c5ed6
abdulquayumhp <badulquayumhp998@gmailc.com>
2022-10-26 01:06:22 +0600

    live data add

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const port = process.env.port || 5000;
 
 const courses = require("./data/corses.json");
 
+app.get("/courses", (req, res) => {
+	res.send(courses);
+});
+
 app.get("/", (req, res) => {
 	res.send("news api running");
 });
